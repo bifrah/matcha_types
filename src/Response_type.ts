67bef043c953b4b3c58948e88afb41c1178107f1
bifrah <benj.ifrah@gmail.com>
2025-07-18 12:49:10 +0200

    add more responses

--- src/Response_type.ts
+++ src/Response_type.ts
@@ -1,9 +1,16 @@
+import { LikeSafeUser, ProfileSafeUser } from './User_type';
 import { Chat } from './Chat';
+import { Views } from './Views_type';
 
 export interface ResponseBody<T> {
   message: string;
   statusCode: number;
   data: T | null;
+}
+
+//Auth controller
+export interface SignupResponse {
+    data: string[] | null;
 }
 
 export interface SigninResponse {
@@ -15,6 +22,43 @@ export interface ValidateUserResponse {
   userUuid: string;
 }
 
+// User/Hobbies Controller
+export interface FindHobbyResponse {
+  uuids: string[];
+}
+
+// User/Notifications
+export interface GetNotificationsResponse {
+  Notifications: Notification[];
+}
+
+export interface PatchNotificationssResponse {
+  messages: string;
+}
+
+// User/Profile Likes Controller
+export interface ProfilesLikesResponse {
+  usersILike: LikeSafeUser[];
+  usersWhoLikeMe: LikeSafeUser[];
+  isAMatch: LikeSafeUser[];
+}
+
+// User/Profile/ View Controller
+export interface ViewsResponse {
+  viewers_uuids: Pick<Views, 'viewerUuid'>[]
+}
+
+// Update Profile
+export interface UpdateResponse {
+  updated_profile: ProfileSafeUser | null;
+}
+
+// Photo Controller
+export interface AddPhotoResponse {
+  photo_uuid: string;
+}
+
+// Chat Controller
 export interface PostMessageResponse {
   messageSent: string;
 }
